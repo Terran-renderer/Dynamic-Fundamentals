e83ae09dc28509d56bb534cf396f6ac1b23baba8
Terran-renderer <Terranrenderer@gmail.com>
2025-07-19 12:56:50 -0600

    finla

--- characterCard/script.js
+++ characterCard/script.js
@@ -8,7 +8,7 @@ function mainLoop() {
         damage:20,
         gold:10,
         image:"img/tankkun.jpg",
-        deck: ["img/Killer-Whale.png", "img/Heal.jpg", "img/punch.jpg", "img/flurry.jpg", "img/cast.jpg"]
+        deck: ["img/Heal.jpg","img/Heal.jpg","img/Heal.jpg","img/Heal.jpg", "img/punch.jpg", "img/flurry.jpg", "img/cast.jpg"]
     };
 
     const Joe = { 
@@ -19,7 +19,7 @@ function mainLoop() {
         damage:120,
         gold:10,
         image:"img/assassain.jpg",
-        deck: ["img/Heal.jpg", "img/punch.jpg", "img/punch.jpg", "img/flurry.jpg", "img/cast.jpg"]
+        deck: ["img/Heal.jpg", "img/punch.jpg", "img/punch.jpg", "img/flurry.jpg", "img/flurry.jpg", "img/flurry.jpg", "img/cast.jpg"]
     };
 
     const Legion = { 
@@ -30,7 +30,7 @@ function mainLoop() {
     damage: 30,
     gold:10,
     image:"img/legion.jpg",
-    deck: ["img/Heal.jpg", "img/punch.jpg", "img/flurry.jpg", "img/cast.jpg", "img/cast.jpg"]
+    deck: ["img/Heal.jpg", "img/punch.jpg", "img/flurry.jpg", "img/cast.jpg", "img/cast.jpg","img/cast.jpg","img/cast.jpg","img/cast.jpg"]
     };
 
     const character = {
@@ -64,7 +64,7 @@ function mainLoop() {
         hideChoices();
         showCard();
         displayCharacter();
-        drawHand(); 
+        startingHand(); 
     }
 
 
@@ -78,7 +78,7 @@ function mainLoop() {
         hideChoices();
         showCard();
         displayCharacter();
-        drawHand();
+        startingHand();
     };
 
     function pickLegion() {
@@ -91,27 +91,9 @@ function mainLoop() {
         hideChoices();
         showCard();
         displayCharacter();
-        drawHand();
+        startingHand();
     };
 
-    function showDeck(){
-        const modal = document.getElementById("deckModal");
-        const modalDeck = document.getElementById("modalDeck");
-        const closeButton = document.getElementById("closeModalButton");
-
-        modalDeck.innerHTML = "";
-
-        character.deck.forEach(cardSrc =>{
-            const tag = document.createElement("img");
-            tag.src = cardSrc;
-            modalDeck.appendChild(tag);
-        });
-
-        modal.showModal();
-
-        closeButton.onclick = () => modal.close();
-
-    }
 
     function displayStartingInfo(){
         const tedContainer = document.getElementById('tedCard');
@@ -139,7 +121,7 @@ function mainLoop() {
                     <p><strong>Class: ${Joe.class}</strong> <span id="class"></span></p>
                     <p><strong>Level: ${Joe.level}</strong> <span id="level"></span></p>
                     <p><strong>Damage: ${Joe.damage}</strong> <span id="damage"></span></p>
-                    <p><strong>Gold: ${Legion.gold}</strong></p> 
+                    <p><strong>Gold: ${Joe.gold}</strong></p> 
 
                 </div>
                 <button id="joeButton">Choose Joe!</button>     
@@ -243,6 +225,34 @@ function mainLoop() {
     let enemy = null;
     document.getElementById("spawnenemies").addEventListener('click', showEnemy);
 
+        function showDeck(){
+        const modal = document.getElementById("deckModal");
+        const modalDeck = document.getElementById("modalDeck");
+        const closeButton = document.getElementById("closeModalButton");
+
+        modalDeck.innerHTML = "";
+
+        character.deck.forEach(cardSrc =>{
+            const tag = document.createElement("img");
+            tag.src = cardSrc;
+            modalDeck.appendChild(tag);
+        });
+
+        modal.showModal();
+
+        closeButton.onclick = () => modal.close();
+
+    }
+
+    function shuffle(deck) {
+        for (let i = deck.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+        return deck;  
+    }
+
+
     function attackCharacter(){
     if (character.health <= 0 || character.level <= 0) {
         alert(character.name + ' has died');
@@ -262,6 +272,127 @@ function mainLoop() {
             };
     };  
 
+    class Deck {
+        constructor(){
+            this.cards = cards
+        }
+    }
+
+    class Card {
+        constructor(cardName){
+            this.cardName = cardName
+            this.image = image
+        }
+    }
+
+    function startingHand() {
+        console.log("Deck before drawing:", deck);
+
+        hand = []; 
+        const handContainer = document.getElementById("handContainer");
+        handContainer.innerHTML = ""; 
+
+        for (let i = 0; i < 4; i++) {
+            
+            if (deck.length === 0 && discard.length > 0) {
+                deck = shuffle([...discard]);
+                discard = [];
+            }
+
+            if (deck.length > 0) {
+                const card = deck.pop();     
+                hand.push(card);             
+
+                const cardImg = document.createElement("img");
+                cardImg.src = card;
+                cardImg.alt = "Card";
+                cardImg.className = "card-image";
+                handContainer.appendChild(cardImg);
+
+                cardImg.addEventListener("click", () => {
+                    playCard(card);
+                });
+            }
+        }
+    }
+
+    function drawCard() {
+        console.log("Deck before drawing:", deck);
+
+        hand = []; 
+        const handContainer = document.getElementById("handContainer");
+        handContainer.innerHTML = ""; 
+            
+            if (deck.length === 0 && discard.length > 0) {
+                deck = shuffle([...discard]);
+                discard = [];
+            }
+
+            if (deck.length > 0) {
+                const card = deck.pop();     
+                hand.push(card);             
+
+                const cardImg = document.createElement("img");
+                cardImg.src = card;
+                cardImg.alt = "Card";
+                cardImg.className = "card-image";
+                handContainer.appendChild(cardImg);
+
+                cardImg.addEventListener("click", () => {
+                    playCard(card);
+                });
+            }
+        }
+
+
+    function playCard(card) {
+        console.log("Played card:", card);
+
+        if (card.includes("punch")) {
+            if (enemy) {
+                enemy.health -= 20;
+                console.log(`${character.name} punches! Enemy health: ${enemy.health}`);
+            }
+        } else if (card.includes("flurry")) {
+            if (enemy) {
+                enemy.health -= 30;
+                console.log(`${character.name} flurries! Enemy health: ${enemy.health}`);
+            }
+        } else if (card.includes("cast")) {
+            if (enemy) {
+                enemy.health -= 40;
+                console.log(`${character.name} casts a spell! Enemy health: ${enemy.health}`);
+            }
+        } else if (card.includes("Heal")) {
+            character.health += 20;
+            console.log(`${character.name} heals! Health: ${character.health}`);
+        }
+
+        const index = hand.indexOf(card);
+        if (index !== -1) {
+            hand.splice(index, 1);
+            discard.push(card);
+        }
+
+        if (enemy && enemy.health <= 0) {
+            console.log("Enemy defeated!");
+            character.gold += enemy.gold;
+            enemy = null;
+            showEnemy(); 
+        } else {
+            showEnemy(); 
+        }
+
+        displayCharacter();
+        drawCard(); 
+    }
+
+
+    function refillHand() {
+        while (hand.length < 4) {
+            drawCard();
+        }
+    }
 
     function displayCharacter() {
         const container = document.getElementById('card');
@@ -281,9 +412,9 @@ function mainLoop() {
         `;
         container.innerHTML = characterInfo;
 
-        document.getElementById('attacked').addEventListener('click', attackCharacter);
+        /*document.getElementById('attacked').addEventListener('click', attackCharacter);
         document.getElementById('heal').addEventListener('click', healCharacter);
-        document.getElementById('levelup').addEventListener('click', levelCharacter);
+        document.getElementById('levelup').addEventListener('click', levelCharacter);*/
 
     };
 
@@ -305,63 +436,11 @@ function mainLoop() {
         show.style.display = 'block';
     };
 
-    function drawHand() {
-        hand = [];
-        const handContainer = document.getElementById("handContainer");
-        handContainer.innerHTML = "";
-
-        for (let i = 0; i < 4; i++) {
-            if (deck.length === 0 && discard.length > 0) {
-                deck = shuffle([...discard]);
-                discard = [];
-            }
-
-            if (deck.length > 0) {
-                const card = deck.pop(); 
-                hand.push(card);
-
-                const cardImg = document.createElement("img");
-                cardImg.src = card;
-                cardImg.alt = "Card";
-                cardImg.className = "card-image"; 
-                handContainer.appendChild(cardImg);
-
-                
-                cardImg.addEventListener('click', () => {
-                playCard(card);
-                });
-            }
-        }
-    };
-
-    function playCard(card) {
-        console.log("Played card:", card);
-        
-        const index = hand.indexOf(card);
-        if (index !== -1) {
-            hand.splice(index, 1);
-            discard.push(card);
-        }
-
-        drawHand();
-    }
-
-    function shuffle(deck) {
-        for (let i = deck.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [deck[i], deck[j]] = [deck[j], deck[i]];
-        }
-        return deck;
-    }
-
-
-    function refillHand(){
-        deck[Math.floor(Math.random() * deck.length)]
-    };
-
     displayStartingInfo();
+    window.showDeck = showDeck;
+    window.startingHand = startingHand;
 };
 
 document.getElementById("startgame").addEventListener('click', mainLoop);
 document.getElementById("showDeckButton").addEventListener("click", showDeck);
-document.getElementById("drawHand").addEventListener("click", drawHand);
+document.getElementById("drawHand").addEventListener("click", startingHand);
